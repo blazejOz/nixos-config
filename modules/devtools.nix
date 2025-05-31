d3004f548f7f3c1539ce33a8752bb05d23660317
blazejOz <blazej.ozog@gmail.com>
2025-05-31 21:38:34 +0200

    added reportlab to python pkgs

--- modules/devtools.nix
+++ modules/devtools.nix
@@ -8,7 +8,7 @@
     pgadmin4
 
     #python:
-    (python3.withPackages (ps: with ps; [ requests matplotlib]))
+    (python3.withPackages (ps: with ps; [ requests matplotlib reportlab]))
 
     gcc
     gdb
