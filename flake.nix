76e0a793e6bc44e15651b0dd56178d2518c354c6
blazejOz <blazej.ozog@gmail.com>
2025-07-18 11:52:18 +0200

    change

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   description = "My NixOS + Home Manager flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      #url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
