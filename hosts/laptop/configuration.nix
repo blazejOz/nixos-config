5033666e8fffc1a7eafc5d2e2604f2054640703b
blazejOz <blazej.ozog@gmail.com>
2025-10-12 10:58:27 +0200

    add(laptop): wireguard client setup for proxmox

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -4,7 +4,21 @@
 
   environment.systemPackages = with pkgs; [
     brightnessctl
+    wireguard-tools
   ];
+
+  #wireguard client setup for proxmox
+  networking.wg-quick.interfaces.wg0 = {
+    address = [ "10.8.0.2/24" ];
+    privateKeyFile = "/etc/wireguard/client_private.key";
+    peers = [{
+      publicKey = "PCqo8T3xMqlQ/VVxiYaG8J/Db/4UeT8s2pHn87L20So=";
+      allowedIPs = [ "192.168.0.0/24" "10.8.0.0/24" ];
+      endpoint = "89.78.139.252:51820";
+      persistentKeepalive = 25;
+    }];
+  };
+
   networking.hostName = "laptop";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
