64cca00ca6cb5805028441df536a8d2440ea3a3f
blazejOz <blazej.ozog@gmail.com>
2025-10-13 08:15:05 +0200

    Add: zerotier vpn tunnel

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -8,16 +8,23 @@
   ];
 
   #wireguard client setup for proxmox
-  networking.wg-quick.interfaces.wg0 = {
-    address = [ "10.8.0.2/24" ];
-    privateKeyFile = "/etc/wireguard/client_private.key";
-    peers = [{
-      publicKey = "PCqo8T3xMqlQ/VVxiYaG8J/Db/4UeT8s2pHn87L20So=";
-      allowedIPs = [ "192.168.0.0/24" "10.8.0.0/24" ];
-      endpoint = "89.78.139.252:51820";
-      persistentKeepalive = 25;
-    }];
-  };
+  # networking.wg-quick.interfaces.wg0 = {
+  #   address = [ "10.8.0.2/24" ];
+  #   privateKeyFile = "/etc/wireguard/client_private.key";
+  #   peers = [{
+  #     publicKey = "PCqo8T3xMqlQ/VVxiYaG8J/Db/4UeT8s2pHn87L20So=";
+  #     allowedIPs = [ "192.168.0.0/24" "10.8.0.0/24" ];
+  #     endpoint = "89.78.139.252:51820";
+  #     persistentKeepalive = 25;
+  #   }];
+  # };
+
+    services.zerotierone = {
+      enable = true;
+      joinNetworks = [
+        "E4DA7455B2ABF745"
+      ];
+    };
 
   networking.hostName = "laptop";
 
