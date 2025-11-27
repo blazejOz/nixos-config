39cb2b3c3477bb930b382ad9638e7dd20e4cec31
blazejOz <blazej.ozog@gmail.com>
2025-11-27 05:19:43 +0100

    remove: old aliases

--- modules/bash.nix
+++ modules/bash.nix
@@ -12,12 +12,6 @@
       #nixos
       flswitch = "sudo nixos-rebuild switch --flake ";
       
-      #server_ip
-      home_server = "ssh blaz@192.168.0.20";
-
-      #Stop/start zerotier vpn tunnel
-      stop_vpn = "sudo systemctl stop zerotierone.service";
-      start_vpn = "sudo systemctl start zerotierone.service";
       
     };
   
